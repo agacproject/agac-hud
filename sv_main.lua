e47119cae75c634adf5a02b52838ba3af8f015ac
deniz <85676222+denizwp@users.noreply.github.com>
2024-07-20 16:56:02 +0300

    data save event title changed (server-side)

--- sv_main.lua
+++ sv_main.lua
@@ -1,7 +1,7 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterServerEvent("bad-hud:savedata:server")
-AddEventHandler("bad-hud:savedata:server", function(yemek, su)
+RegisterServerEvent("agac-hud:savedata:server")
+AddEventHandler("agac-hud:savedata:server", function(yemek, su)
     local src = source
     local xPlayer = QBCore.Functions.GetPlayer(src)	
     if xPlayer then
